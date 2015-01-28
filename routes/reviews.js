f59876e520b445064b618cbf8c6b1f463411997e
julienkosinski <julien.kosinski@gmail.com>
2015-01-28 14:33:49 +0100

    Add get, post, delete to reviews.js.
    Fix integration of the route.

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,3 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var reviewsTest = [{name:"McDo", placeType:"Fastfood", stars: 3}, {name:"McDo", placeType:"Fastfood", stars: 3}];
+
+router.get('/', function (req, res) {
+	res.send(reviewsTest);
+});
+
+router.post('/', function (req, res) {
+	res.send('OK');
+});
+
+router.delete('/', function (req, res) {
+	res.send('OK');
+});
+module.exports = router;
