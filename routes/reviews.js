a48eef865d1258ba205fc5bbe23c659f1d64e152
julienkosinski <julien.kosinski@gmail.com>
2015-02-17 14:45:40 +0100

    Browser front-end to add a review

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,68 +5,96 @@ var reviewsSchema = require('../database/schema');
 //var reviewsTest = [{name:"McDo", placeType:"Fastfood", stars: 3}, {name:"McDo", placeType:"Fastfood", stars: 3}];
 
 router.route('/')
-.get(function (req, res) {
-	reviewsSchema.find({}, function (err, reviews) {
-		if (err) {
-			res.status(500).send({'error': err});
-		} else {
-			if (req.get('Accept').toString().match(/html/)) {
-				res.render('reviews', {reviews: reviews});
+	.get(function (req, res) {
+		reviewsSchema.find({}, function (err, reviews) {
+			if (err) {
+				res.status(500).send({'error': err});
+			} else {
+				if (req.get('Accept').toString().match(/html/)) {
+					res.render('reviews', {reviews: reviews});
+				}
+				else {
+					res.send(reviews);
+				}
 			}
-			else {
-				res.send(reviews);
-			}
-		}
-	});
-});
+		})
+	})
 
-router.route('/')
-.post(function (req, res) {
-	if(!req.body.name || !req.body.placeType || !req.body.stars) {
-		res.status(400).send("Please enter every parameters required.");
-	} else {
-		var review = {
-			name: req.body.name,
-			placeType: req.body.placeType,
-			stars: req.body.stars
-		};
-		reviewsSchema.create(review, function (err, review) {
+	.post(function (req, res) {
+		if(!req.body.name || !req.body.placeType || !req.body.stars) {
+			res.status(400).send("Please enter every parameters required.");
+		} else {
+			var review = {
+				name: req.body.name,
+				placeType: req.body.placeType,
+				stars: req.body.stars
+			};
+			reviewsSchema.create(review, function (err, review) {
+				if(err){
+					res.status(500);
+				}
+				else{
+					res.status(201);
+					if (req.get('Accept').toString().match(/html/)) {
+						res.redirect("/reviews/"+review.id)
+					}
+					else {
+						res.send(reviews);
+					}
+				}
+			});
+		}
+	})
+
+	.delete(function (req, res) {
+		reviewsSchema.remove(function (err) {
 			if(err){
-				res.status(500);
+				res.status(500).send();
 			}
 			else{
-				res.status(201);
-				res.send(review);
+				res.status(204).send();
 			}
 		});
-	}
-});
+	})
+;
 
-router.delete('/', function (req, res) {
-	reviewsSchema.remove(function (err) {
-		if(err){
-			res.status(500).send();
+router.route('/add') 
+	.get(function (req, res) {
+		console.log('here');
+		if (req.get('Accept').toString().match(/html/)) {
+			res.render('review-add');
 		}
-		else{
-			res.status(204).send();
+		else {
+			res.status(400).send("This view is browser specific!");
 		}
-	});
-});
+	})
+;
 
-router.get('/:id', function (req, res) {
-	reviewsSchema.findOne({_id: req.params.id}, function (err, review) {
-		if(err){
-			res.send(404);
-		} else {
-			if (req.get('Accept').toString().match(/html/)) {
-				res.render('review', {review: review});
+router.route('/:id') 
+	.get( function (req, res) {
+		reviewsSchema.findOne({_id: req.params.id}, function (err, review) {
+			if(err){
+				res.send(404);
+			} else {
+				if (req.get('Accept').toString().match(/html/)) {
+					res.render('review', {review: review});
+				}
+				else {
+					res.send(review);
+				}
 			}
-			else {
-				res.send(review);
+		});
+	})
+	.put(function(req, res){
+		reviewsSchema.findOneAndUpdate({_id: req.params.id}, req.body, function (err, review) {
+			if(err) {
+				res.send(404);
+			} else {
+				res.status(201).send(review);
 			}
-		}
+		});
 	});
-});
+
 
 router.put('/:id', function(req, res){
 	reviewsSchema.findOneAndUpdate({_id: req.params.id}, req.body, function (err, review) {
